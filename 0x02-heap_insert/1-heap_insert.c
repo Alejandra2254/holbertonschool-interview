0bbc43e6577ee66659a535256510b28b78d929e8
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-12-22 13:31:30 -0600

    Adding task 1

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 /**
-* binary_tree_size -measures the size of a binary tree
-* @tree:  pointer to the root node of the tree to measure the size.
-* Return: If tree is NULL, function return 0
-**/
+ * binary_tree_size -measures the size of a binary tree
+ * @tree:  pointer to the root node of the tree to measure the size.
+ * Return: If tree is NULL, function return 0
+ **/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t bt_size = 0;
