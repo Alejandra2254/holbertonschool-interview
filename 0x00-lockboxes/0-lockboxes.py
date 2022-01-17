8d2f99fa9b8294f44a7df19fea64391a601a92bc
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2022-01-17 14:51:20 -0600

    Update 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,16 +2,48 @@
 """can unlock all boxes """
 
 
+#!/usr/bin/python3
+
 def canUnlockAll(boxes):
-    newlist = []
-    k = len(boxes)
-    for i in boxes:
-        if len(i) == 0 and i is not boxes[k-1]:
-            return False
-        for j in i:
-            newlist.append(j)
-    for index, keys in enumerate(boxes):
-        if index in newlist or index < k-1:
-            return True
-        else:
-            return False
+    if not boxes or ( type(boxes) != list):
+        return 0
+    keyList=[]
+    hasMoreKeys = False
+    countBoxes = len(boxes)
+    boxesUnlock = setBoxesUnlock(countBoxes)
+   
+    for i in range(countBoxes):
+        if( type(boxes[i]) == list ):
+            if i == 0:
+                keyList.append(0)
+                setKeys(boxes[i], keyList)
+           
+            #print("i: {}, boxesUnlock[i]: {}, keyList: {}".format(i, boxesUnlock,keyList))
+            if boxesUnlock[i] == 0 and keyList.count(i) > 0:
+                #print("i: {}, boxesUnlock: {}, keyList: {}".format(i,boxesUnlock,keyList))
+                unlockBox(keyList,boxesUnlock,boxes)
+                #print("i: {}, boxesUnlock: {}, keyList: {}".format(i,boxesUnlock,keyList))
+
+            if boxesUnlock.count(0) == 0:
+                return True
+    
+    return False
+
+def unlockBox(keys,boxesUnlock,boxes):
+    keys_copy = keys
+    for key in keys_copy:
+        if key <= len(boxesUnlock):
+            boxesUnlock[key] = 1
+            setKeys(boxes[key],keys)
+        keys.remove(key)
+
+def setKeys(box, keyList):
+    for key in box:
+        if keyList.count(key) == 0:
+            keyList.append(key) 
+
+def setBoxesUnlock(count):
+    boxes = []
+    for i in range(count):
+        boxes.append(0)
+    return boxes       
